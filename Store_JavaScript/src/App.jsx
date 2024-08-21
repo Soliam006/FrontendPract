6c3e63ef98831a9ad1e2939d2e8fcf0ee35cbf6b
Soliam006 <wsottoja7@alumnes.ub.edu>
2024-08-21 23:25:11 +0200

    Responsive Menu. Movil Menu Begin

--- Store_JavaScript/src/App.jsx
+++ Store_JavaScript/src/App.jsx
@@ -1,11 +1,35 @@
 import SideBar from './components/share/SideBar'
+import { MdRestaurantMenu, MdOutlineShoppingCart, MdOutlinePersonOutline   } from "react-icons/md";
+import {useState} from "react";
 function App() {
-
-  return (
+    const [showMenu, setShowMenu] = useState(false)
+    const [showOrder, setShowOrder] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+    return (
         <div className="bg-[#272727] w-full min-h-screen ">
-            <SideBar/>
-        </div>
-)
+            <SideBar showMenu={showMenu} />
+            {/* Movil Menu */}
+            <nav className="bg-[#000e14] fixed w-full lg:hidden bottom-0 left-0 flex flex-row justify-between p-4 text-3xl rounded-t-lg">
+                <button onClick={toggleMenu}
+                    className="flex flex-col items-center justify-between px-4 p-3 group rounded-full hover:bg-[#dd7d36] transition-colors">
+                    <MdRestaurantMenu className=" text-[#dd7d36]  group-hover:text-white"/>
+                    <span className="text-white hidden group-hover:block text-2xl ">Menu </span>
+                </button>
+                <button className="flex flex-col items-center justify-between px-4 py-3 group rounded-full hover:bg-[#dd7d36] transition-colors">
+                    <MdOutlineShoppingCart className=" text-[#dd7d36]  group-hover:text-white"/>
+                    <span className="text-white hidden group-hover:block text-2xl  ">Shop </span>
+                </button>
+                <button className="flex flex-col items-center justify-between px-4 py-3 group rounded-full hover:bg-[#dd7d36] transition-colors">
+                    <MdOutlinePersonOutline  className=" text-[#dd7d36]  group-hover:text-white"/>
+                    <span className="text-white hidden group-hover:block text-2xl ">Account </span>
+                </button>
+            </nav>
 
-  }
-    export default App
+        </div>
+  )
+
+}
+
+export default App
--- Store_JavaScript/src/components/share/SideBar.jsx
+++ Store_JavaScript/src/components/share/SideBar.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import { MdOutlineFoodBank  } from 'react-icons/md';
-const SideBar = () => {
+import { MdOutlineFoodBank, MdOutlineLogout,  MdContactPhone, MdOutlineSettingsSuggest , MdNotificationsNone     } from 'react-icons/md';
+const SideBar = (props) => {
+    const {showMenu} = props;
+
     return (
-        <div className="bg-[#000e14] fixed left-0 top-0 pt-5 w-28 h-full">
-            <h1 className="text-2xl text-gray-300 uppercase font-bold text-center">Logo</h1>
-            <ul className="mt-10 pl-4">
-                <li className=" bg-[#272727]  p-4 rounded-tl-xl rounded-bl-xl">
-                    <a href="#" className="text-white bg-[#dd7d36] block rounded-xl  p-4 text-center">
-                        <MdOutlineFoodBank className="text-3xl  "/>
+        // Flex Class that allow to put the content in a column, with that the logout it's at the bottom
+        <div className={`bg-[#000e14] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between
+        py-3 rounded-tr-xl rounded-br-xl  ${showMenu ? "left-0" : "-left-full"} transition-all`}>
+            <div>
+                <ul className="mt-5 pl-4 ">
+                    <li>
+                        <h1 className="text-xl text-gray-300 uppercase font-bold text-center">Logo</h1>
+                    </li>
+                    <li className=" bg-[#272727]  p-4 rounded-tl-xl rounded-bl-xl">
+                        <a href="#" className="text-white bg-[#dd7d36] flex justify-center rounded-xl  p-4 text-center">
+                            <MdOutlineFoodBank className="text-3xl  "/>
                         </a>
-                </li>
-                <li className="mb-4">
-                    <a href="#" className="block text-center">
-                        About</a>
-                </li>
-                <li className="mb-4">
-                    <a href="#" className="text-gray-300 hover:text-white text-sm block text-center">
-                        Services</a>
-                </li>
-                <li className="mb-4">
-                    <a href="#" className="text-gray-300 hover:text-white text-sm block text-center">
-                        Contact</a>
-                </li>
-            </ul>
+                    </li>
+                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                        <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
+                    flex justify-center rounded-xl  p-4 text-center transition-colors">
+                            <MdNotificationsNone className="text-3xl  "/>
+                        </a>
+                    </li>
+                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                        <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
+                    flex justify-center rounded-xl  p-4 text-center transition-colors">
+                            <MdContactPhone className="text-3xl  "/>
+                        </a>
+                    </li>
+                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                        <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
+                    flex justify-center rounded-xl  p-4 text-center transition-colors">
+                            <MdOutlineSettingsSuggest className="text-3xl  "/>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+            <div>
+                <ul className="mt-5 pl-4">
+                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                        <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
+                    flex justify-center rounded-xl  p-4 text-center transition-colors">
+                            <MdOutlineLogout  className="text-3xl  "/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
