435b8184132e6f17dabd5d41e5d87662d4a600b6
Soliam006 <wsottoja7@alumnes.ub.edu>
2024-08-22 13:17:54 +0200

    Tap navegation Added (Dishes and Desserts)

--- Store_JavaScript/src/App.jsx
+++ Store_JavaScript/src/App.jsx
@@ -1,14 +1,38 @@
 import SideBar from './components/share/SideBar'
-import { MdRestaurantMenu, MdOutlineShoppingCart, MdOutlineClose , MdOutlinePersonOutline   } from "react-icons/md";
+import { MdRestaurantMenu, MdOutlineShoppingCart, MdOutlineSearch , MdOutlineClose , MdOutlinePersonOutline   } from "react-icons/md";
 import {useState} from "react";
+import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 function App() {
+    /*Menu Movil Information*/
     const [showMenu, setShowMenu] = useState(false)
     const [showOrder, setShowOrder] = useState(false)
     const toggleMenu = () => {
         setShowMenu(!showMenu)
     }
+    /*Date and User Information*/
+    const [userName, setUserName] = useState("User Name")
+    const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentDate(new Date().toLocaleDateString('en-En', {
+                weekday: 'long',
+                month: 'long',
+                year: 'numeric',
+                day: 'numeric'
+            }));
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+    {/*Tabs Navegation Data*/}
+    const [activeTab, setActiveTab] = useState("Hot Dishes")
+    const tabs = ["Hot Dishes", "Cold Dishes", "Desserts", "Drinks"]
     return (
-        <div className="bg-[#272727] w-full min-h-screen ">
+        <div className="bg-[#1e1e24] w-full min-h-screen ">
             <SideBar showMenu={showMenu} />
             {/* Movil Menu */}
             <nav className="bg-[#000e14] fixed w-full lg:hidden bottom-0 left-0 flex flex-row justify-between p-4 text-3xl rounded-t-lg">
@@ -32,10 +56,37 @@ function App() {
             {/*Main container con un pl de 28 para evitar el sideBar*/}
             {/*grid-cols-1 (Ocupa toda la pantalla) | Grid-cols-8 lg (Para Desktop la divido en 8)*/}
             <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
-                <div className="lg:col-span-6 bg-[#272727]">
-                    <header className="bg-[#272727] p-4">
-                        <h1>User Experience</h1>
-                        <p>Fecha actual</p>
+                <div className="lg:col-span-6 bg-[#1e1e24]">
+                    {/*Header*/}
+                    <header className="bg-[#1e1e24] p-4">
+                        {/*Title and Searchj*/}
+                        <div className="flex flex-row gap-2 justify-between">
+                            <div>
+                                <h1 className="text-2xl text-gray-300">{userName}</h1>
+                                <p className="text-gray-500">{currentDate}</p>
+                            </div>
+                            <form >
+                                <div className="w-full relative">
+                                      <MdOutlineSearch className="left-4 top-1/2 -translate-y-1/2 text-white absolute"/>
+                                <input type="text" placeholder="Search" className="pl-10 py-2 pr-4 bg-[#000e14] w-full text-gray-300 rounded-lg outline-none"/>
+                                </div>
+                            </form>
+                        </div>
+                        {/*Tabs*/}
+                        <nav className="flex flex-row justify-between border-b-1 border-[#ffffff] w-full mt-6 mb-6 ">
+                            {tabs.map((tab) => (
+                                <motion.button key={tab}
+                                        onClick={() => setActiveTab(tab)}
+                                        className={`py-2 px-3  ${activeTab === tab ? "text-[#dd7d36]" : "text-gray-300"}
+                                                                                 ${activeTab === tab ? "border-b-2 border-[#dd7d36]": ""} font-sans font-bold text-sm`}
+                                        whileTap={{ scale: 0.9 }}
+                                >{tab}
+                                </motion.button>
+                            ))}
+                        </nav>
+                        <div>
+                            <h2 className="text-gray-500 text-xl font-sans font-bold">Choose your {activeTab}</h2>
+                        </div>
                     </header>
                 </div>
                 <div className="lg:col-span-2 bg-[#000e14] fixed lg:static right-0">Hola2</div>
--- Store_JavaScript/src/components/share/SideBar.jsx
+++ Store_JavaScript/src/components/share/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdOutlineFoodBank, MdOutlineLogout,  MdContactPhone, MdOutlineSettingsSuggest , MdNotificationsNone     } from 'react-icons/md';
+import { MdOutlineFoodBank, MdOutlineLogout,  MdOutlinePersonOutline, MdOutlineSettingsSuggest , MdNotificationsNone     } from 'react-icons/md';
 const SideBar = (props) => {
     const {showMenu} = props;
 
@@ -12,24 +12,24 @@ const SideBar = (props) => {
                     <li>
                         <h1 className="text-xl text-gray-300 uppercase font-bold text-center">Logo</h1>
                     </li>
-                    <li className=" bg-[#272727]  p-4 rounded-tl-xl rounded-bl-xl">
+                    <li className=" bg-[#1e1e24]  p-4 rounded-tl-xl rounded-bl-xl">
                         <a href="#" className="text-white bg-[#dd7d36] flex justify-center rounded-xl  p-4 text-center">
                             <MdOutlineFoodBank className="text-3xl  "/>
                         </a>
                     </li>
-                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                    <li className=" hover:bg-[#1e1e24] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
                         <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
                     flex justify-center rounded-xl  p-4 text-center transition-colors">
                             <MdNotificationsNone className="text-3xl  "/>
                         </a>
                     </li>
-                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                    <li className=" hover:bg-[#1e1e24] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
                         <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
                     flex justify-center rounded-xl  p-4 text-center transition-colors">
-                            <MdContactPhone className="text-3xl  "/>
+                            <MdOutlinePersonOutline className="text-3xl  "/>
                         </a>
                     </li>
-                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                    <li className=" hover:bg-[#1e1e24] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
                         <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
                     flex justify-center rounded-xl  p-4 text-center transition-colors">
                             <MdOutlineSettingsSuggest className="text-3xl  "/>
@@ -40,7 +40,7 @@ const SideBar = (props) => {
             </div>
             <div>
                 <ul className="mt-5 pl-4">
-                    <li className=" hover:bg-[#272727] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+                    <li className=" hover:bg-[#1e1e24] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
                         <a href="#" className="group-hover:bg-[#dd7d36] text-[#dd7d36] group-hover:text-white
                     flex justify-center rounded-xl  p-4 text-center transition-colors">
                             <MdOutlineLogout  className="text-3xl  "/>
